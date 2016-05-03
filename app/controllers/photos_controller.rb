b302393fb1d44b95762b7a90eb282e243f66b032
maggiehsu <maggiehsu@users.noreply.github.com>
2016-05-03 18:43:54 -0500

    maggie

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,41 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    p = Photo.find_by({:id=>params[:id]})
+    @photo_source = p.source
+    @photo_caption = p.caption
+    @photo_id = p.id
+  end
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:caption]
+    p.source = params[:source]
+    p.save
+    redirect_to("http://localhost:3000/photos")
+
+  end
+  def destroy
+    p = Photo.find_by({:id=>params[:id]})
+    p.destroy
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({:id=>params[:id]})
+  end
+
+  def update_row
+    p = Photo.find_by({:id=>params[:id]})
+    p.caption= params[:new_caption]
+    p.source= params[:new_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos/"+params[:id])
+  end
 
 end
