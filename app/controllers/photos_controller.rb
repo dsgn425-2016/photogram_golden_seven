581c4f12718141df256914d07bd2e626580e6a3f
Ahalya Vijay <avijay2017@users.noreply.github.com>
2016-05-04 10:13:06 -0500

    create completed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,9 +5,21 @@ class PhotosController < ApplicationController
 
   def show
     @photo = Photo.find_by({ :id => params[:id]})
-    # @photo_source = @photo.source
-    # @photo_caption = @photo.caption
-    # @photo_id = @photo.id
+    @photo_source = @photo.source
+    @photo_caption = @photo.caption
+    @photo_id = @photo.id
+  end
+
+  def new_form
+
+  end
+
+  def create_photo
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    redirect_to("/photos")
   end
 
 end
