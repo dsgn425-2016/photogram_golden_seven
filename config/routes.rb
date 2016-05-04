5b3ea1129f9433cc4f302fc9db63183fca5b3c71
ad1441 <adivakaran2017@kellogg.northwestern.edu>
2016-05-04 09:31:30 -0500

    anil-d-for-review

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
+  get("/photos/new",       { :controller => "photos", :action => "new_form" })
+  get("/photos/delete_photo/:id",       { :controller => "photos", :action => "delete" })
+  get("/photos/create_photo",       { :controller => "photos", :action => "create" })
+  get("/photos/:id/edit",       { :controller => "photos", :action => "edit_form" })
+  get("/photos/update_photo/:id",       { :controller => "photos", :action => "update" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+
 
 end
