abcb07264bee282a024f568f8267728754674aa7
Bo Gustafsson <bgustafsson2017@>
2016-05-05 15:31:22 -0500

    still need to create edits

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,32 @@ class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
   end
-
-
+  def show
+    @photo = Photo.find_by({ :id => params[:id] })
+    render ("show.html.erb")
+  end
+def new_form
+  render("new_form.html.erb")
 end
+def create_row
+  p = Photo.new
+  p.caption = params[:the_caption]
+  p.source = params[:the_source]
+  p.save
+
+  #render("create_row.html.erb")
+  redirect_to("http://localhost:3000/photos")
+end
+def destroy
+  p = Photo.find_by({ :id => params[:id] })
+  p.destroy
+  redirect_to("http://localhost:3000/photos")
+end
+def edit
+  p = Photo.find_by({ :id => params[:id] })
+  p.caption = params[:the_caption]
+  p.source = params[:the_source]
+  p.save
+  render("edit.html.erb")
+end
+  end
