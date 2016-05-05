9a670550fb5c49118ce6c53dcc8843c852980f02
vsathpathy <vsathpathy2017@kellogg.northwestern.edu>
2016-05-05 16:26:41 -0500

    Done
    Need to check

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,51 +1,44 @@
 class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
-    render("index.html.erb")
   end
 
   def show
-    @photoid=params[:id]
-    @photoshow= Photo.find(@photoid)
-      render("show.html.erb")
+      @photo = Photo.find(params[:id])
   end
 
   def newform
-    render("newphoto.html.erb")
+
   end
 
 def newphoto
-  p = Photo.new
-      p.caption =params[:the_caption]
-          p.source =params[:the_source]
-                p.save
-              @list_of_photos=Photo.all
-                render("index.html.erb")
+  @photo = Photo.new
+      @photo.caption =params[:the_caption]
+          @photo.source =params[:the_source]
+                @photo.save
+                @list_of_photos = Photo.all
+                render("index")
 end
 
   def destroy
-    @photoid=params[:id]
-    @photodel = Photo.find(@photoid)
-    @photodel.destroy
-    @list_of_photos=Photo.all
-      render("index.html.erb")
-      end
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    @list_of_photos = Photo.all
+    render("index")
+        end
 
 
       def edit_form
-        @photoidedit=params[:id]
-        @photoedit = Photo.find(@photoidedit)
-        render("editphoto.html.erb")
+        @photoedit = Photo.find(params[:id])
+
       end
 
 
       def update_row
-        @photoidedited=params[:id]
-        @photoedit1 = Photo.find(@photoidedited)
-        @photoedit1.caption = params[:the_caption]
-            @photoedit1.source = params[:the_source]
-                  @photoedit1.save
-                  @photoshow= Photo.find(@photoidedited)
-                    render("show.html.erb")
+        @photo = Photo.find(params[:id])
+        @photo.caption = params[:the_caption]
+            @photo.source = params[:the_source]
+                  @photo.save
+                  render("show")
       end
 end
