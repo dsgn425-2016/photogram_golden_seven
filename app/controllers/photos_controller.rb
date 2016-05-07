3827d5c74240747352f4428d11545fb09b5082d7
rsu121 <rsuzuki2017@users.noreply.github.com>
2016-05-07 13:17:42 -0500

    changed params and links

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,8 +8,8 @@ class PhotosController < ApplicationController
 
   def create_row
     p = Photo.new
-    p.caption = params[:caption]
-    p.source = params[:source]
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
     p.save
     redirect_to("http://localhost:3000/photos")
   end
@@ -24,10 +24,10 @@ class PhotosController < ApplicationController
 
   def update_row
     p = Photo.find_by ({ :id => params[:id] })
-    p.caption = params[:caption]
-    p.source = params[:source]
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
     p.save
-    redirect_to("http://localhost:3000/photos")
+    redirect_to("http://localhost:3000/photos/" + params[:id])
   end
 
   def destroy
