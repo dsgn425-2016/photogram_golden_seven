d2bf8f4ca6637442236c628c04dcd3e2533b851d
jillseidel <jseidel2017@users.noreply.github.com>
2016-05-07 07:34:30 -0500

    through end of video

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,17 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+def show
+  @photo = Photo.find_by({:id => params[:id]})
+end
+
+def create_row
+p = Photo.new
+p.caption = params[:the_caption]
+p.source = params[:the_source]
+p.save
+
+redirect_to("http://localhost:3000/photos")
+end
 
 end
