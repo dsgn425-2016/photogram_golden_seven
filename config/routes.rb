4abd5d556efa5e33cf1fc89259c6df0ee60f3b6d
mbhangley1 <mbhangley2017@kellogg.northwestern.edu>
2016-05-08 17:31:10 -0500

    Attempt 2
    Deleted additional lines of code

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
-
-  # Routes to READ photos
-  get("/photos",     { :controller => "photos", :action => "index" })
   get("/photos/:id",  { :controller => "photos", :action => "show" })
-  get("/photos",     { :controller => "photos", :action => "index" })
   get("/photos/new", { :controller => "photos", :action => "new_form" })
   get("/photos/delete_photo/:id", { :controller => "photos", :action => "delete" })
   get("/photos/create_photo",{ :controller => "photos", :action => "create" })
   get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
   get("/photos/update_photo/:id", { :controller => "photos", :action => "update" })
-  get("/photos/:id",{ :controller => "photos", :action => "show" })
+
 
 
 end
