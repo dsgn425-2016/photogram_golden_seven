eeca8a42bf54c3a66c3193f65d8351392acbcd1b
J <B>
2016-05-08 17:22:06 -0500

    view done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,16 @@
 class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
+
   end
 
+  def show
+    @bigimage = Photo.find_by({ :id => params[:id]})
+    render ("show.html.erb")
+  end
+
+  def new_form
+      render ("new.html.erb")
+  end
 
 end
