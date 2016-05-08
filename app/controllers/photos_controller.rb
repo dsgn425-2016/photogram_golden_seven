8d076d8cbd4323047c7d24068f553b5a94b51cb2
mbhangley1 <mbhangley2017@kellogg.northwestern.edu>
2016-05-08 17:26:55 -0500

    Attempt 1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,41 @@
 class PhotosController < ApplicationController
+
   def index
     @list_of_photos = Photo.all
+  end
+
+  def new_form
+  render (new.html.erb)
+  end
+
+  def create_row
+    new_row = Photo.new
+    new_row.source = params["the source"]
+    new_row.caption = params["the caption"]
+    new_row.save
+    redirect_to("http://localhost.3000/photos")
+  end
+
+  def show
+      @current_photo = Photo.find(params[:id])
+  end
+
+  def delete
+   @current_photo = Photo.find(params[:id])
+   @current_photo.destroy
+   redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @current_photo = Photo.find(params[:id])
+  end
+
+  def update
+    @current_photo = Photo.find(params[:id])
+    @current_photo.caption = params["the_caption"]
+    @current_photo.source = params["the_source"]
+    @current_photo.save
+    redirect_to("http://localhost:3000/photos")
   end
 
 
