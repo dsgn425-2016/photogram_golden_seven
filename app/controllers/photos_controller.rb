1589676411ce7ddd316720020c97ba94866492d7
jgoldstein2017 <jgoldstein2017@kellogg.northwestern.edu>
2016-05-08 21:13:18 -0500

    almost-done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -20,4 +20,24 @@ class PhotosController < ApplicationController
   def new_form
 
   end
+
+  def destroy
+    p = Photo.find_by({ :id => params[:id] })
+    p.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({ :id => params[:id] })
+  end
+
+  def update_row
+    p = Photo.find_by({ :id => params[:id] })
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
 end
