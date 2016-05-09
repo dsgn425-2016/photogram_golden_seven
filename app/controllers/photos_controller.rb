77bacf64db4c921f1c21dc07c8ffbb8186de1c2e
aniketaj <ajoglekar2017@kellogg.northwestern.edu>
2016-05-08 22:05:23 -0500

    Stopping point 1
    Almost there

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,37 @@
 class PhotosController < ApplicationController
+
+
   def index
     @list_of_photos = Photo.all
   end
 
+def show
+  @photo=Photo.find_by({:id=>params[:id]})
+end
 
+def new_form
+
+
+end
+
+def create_row
+p=Photo.new
+p.caption=params[:the_caption]
+p.source=params[:the_source]
+p.save
+redirect_to("http://localhost:3000/photos")
+end
+
+def destroy
+p=Photo.find_by({:id=>params[:id]})
+  p.destroy
+  p.save
+  redirect_to("http://localhost:3000/photos")
+end
+
+def edit_form
+  # p=Photo.find_by({:id=>params[:id]})
+
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
+# for creating photos
+  get("/photos/new", {:controller=>"photos", :action=>"new_form"})
+  get("/create_photo", {:controller=>"photos", :action=>"create_row"})
+
+# to delete photos
+get("/delete_photo/:id", {:controller=>"photos", :action=>"destroy"})
+
+#to edit photos
+get("/photos/:id/edit", {:controller=>"photos", :action=>"edit_form"})
+
+# to update photos
+get("/update_photos/:id", {:controller=>"photos", :action=>"update_row"})
+
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
