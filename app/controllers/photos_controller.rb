90760eb720a8a0dc092f55b7221421c27e6738b6
blauwald <jpark2017@kellogg.northwestern.edu>
2016-05-09 13:09:21 -0500

    ready to review

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,45 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id] })
+  end
+
+  def edit_form
+    @photoedit = Photo.find(params[:id])
+  end
+
+  def update_row
+    p = Photo.find(params[:id])
+    p.caption = params[:the_cation]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+
+  end
+
+  def delete
+
+    p = Photo.find(params[:id])
+    p.destroy
+    p.save
+
+    # render("create-row.htnl.erb")
+    redirect_to("http://localhost:3000/photos")
+
+  end
+
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_cation]
+    p.source = params[:the_source]
+    p.save
+
+    # render("create-row.htnl.erb")
+    redirect_to("http://localhost:3000/photos")
+  end
+
 
 end
