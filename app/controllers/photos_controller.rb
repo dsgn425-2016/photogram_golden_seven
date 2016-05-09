f4ac5375f010666064a426d59222288acd85e81b
Jingjianxu <daniel.xujingjian@hotmail.com>
2016-05-08 20:16:50 -0500

    ready for feedback

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,41 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+def show
+  @photo = Photo.find_by({ :id => params[:id] })
+  end
+
+def new_form
+
+end
+
+def create_row
+p = Photo.new
+p.caption = params[:the_caption]
+p.source = params[:the_source]
+p.save
+
+#render("create_row.html.erb")
+
+redirect_to("http://localhost:3000/photos")
+end
+
+def destroy
+  @photo = Photo.find_by({ :id => params[:id] })
+  @photo.destroy
+  redirect_to("http://localhost:3000/photos")
+end
+
+def edit_form
+  @photo = Photo.find_by({ :id => params[:id] })
+end
+
+def update_row
+  @photo = Photo.find_by({ :id => params[:id] })
+  @photo.source = params["the_source"]
+  @photo.caption = params["the_caption"]
+  @photo.save
+  redirect_to("http://localhost:3000/photos")
+end
 
 end
