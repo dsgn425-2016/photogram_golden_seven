8a29fadcdc6e5f305ac6f701540b171a47961137
sof-am <sofiamachline1@dhcp-10-105-122-248.wireless.northwestern.private>
2016-05-09 13:05:42 -0500

    final

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -34,12 +34,12 @@ class PhotosController < ApplicationController
 
 
   def update_row
-    p = Photo.find_by ({ :id => params[:id] })
-    p.caption = params[:the_caption]
-    p.source = params[:the_source]
-    p.save
     @photo = Photo.find_by ({ :id => params[:id] })
-    redirect_to("http://localhost:3000/photos/<%= @photo.id %>")
+    @photo.caption = params[:the_caption]
+    @photo.source = params[:the_source]
+    @photo.save
+
+    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   #Route to  edit
   get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
-  get("/update_photo", { :controller => "photos", :action => "update_row" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
