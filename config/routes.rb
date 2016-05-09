b16dfce788b2c1f3d422e34d5968fe9b427ec4e5
sof-am <sofiamachline1@air-de-sofia.attlocal.net>
2016-05-08 23:07:10 -0500

    I can't find my error

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  #Routes to create photos
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("/create_photo", { :controller => "photos", :action => "create_row" })
+
+  #Route do delete
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+
+  #Route to  edit
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo", { :controller => "photos", :action => "update_row" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
