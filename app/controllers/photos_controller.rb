54304a94055894349b78dd6fba287ebf38891253
Laura Martin <laura.marie.martin@gmail.com>
2016-05-09 13:37:56 -0500

    Completed Photgram

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,41 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id] })
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    @photo = Photo.find_by({ :id => params[:id] })
+    @photo.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo = Photo.find_by({ :id => params[:id] })
+  end
+
+  def update_row
+    @photo = Photo.find_by({ :id => params[:id] })
+    @photo.caption = params[:edit_caption]
+    @photo.source = params[:edit_source]
+    @photo.save
+
+    redirect_to("http://localhost:3000/photos/"+@photo.id.to_s)
+  end
 
 end
