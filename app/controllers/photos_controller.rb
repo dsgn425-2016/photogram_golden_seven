fca58cebd7408f96de55d04f6cc4687050900372
Anita Prasad <aprasad2017@kellogg.northwestern.edu>
2016-05-09 02:15:14 -0500

    More video updates

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,11 +4,20 @@ class PhotosController < ApplicationController
   end
 
   def show
-    photo = Photo.find_by ({:id => params[:id]})
-    @photo_source = photo.source
-    @photo_caption = photo.caption
-    @photo_id = photo.id
+    @photo = Photo.find_by ({:id => params[:id]})
+  end
 
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
   end
 
 end
