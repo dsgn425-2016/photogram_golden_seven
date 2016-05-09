8b9defcf01e8821f29dbd2a60f7240f7b96884d8
FamaLJ <blongjohn2017@kellogg.northwestern.edu>
2016-05-09 06:37:03 -0500

    note

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
-  # Routes to READ photos
+  # Routes to READ photos and determine actions
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
