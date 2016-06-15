da927e0f77c5a6711961591b1146e0c92c811bb2
ChristineYYChen <ychen2017@kellogg.northwestern.edu>
2016-06-15 18:34:39 -0500

    Final Project
    My final project

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,24 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+def show
+p = Photo.find_by({:id => params[:id]})
+@photo_caption = p.source
+@photo_source = p.caption
+@photo_id = p.id
+@photo_id = p.id
+
+end
+
+def new_form
+end
+
+def create_row
+  p = Photo.new
+  p.caption = params[:the_caption]
+  p.source = params[:the_source]
+  p.save
+
+end
 
 end
